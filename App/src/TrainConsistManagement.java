bb1546e946805e33c5e64e36f749dad3ce2c8731
purushotham <purushotham10082.indus@gmail.com>
2026-04-02 14:59:51 +0530

    Track Unique Bogie IDs (Set – HashSet)

--- App/src/TrainConsistManagement.java
+++ App/src/TrainConsistManagement.java
@@ -1,30 +1,19 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class TrainConsistManagement {
 
     public static void main(String[] args) {
 
-        ArrayList<String> passengerBogies = new ArrayList<>();
+        HashSet<String> bogieIds = new HashSet<>();
 
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101");
+        bogieIds.add("BG104");
+        bogieIds.add("BG102");
 
-        System.out.println("Passenger Bogies after addition:");
-        System.out.println(passengerBogies);
-
-        passengerBogies.remove("AC Chair");
-
-        System.out.println("\nPassenger Bogies after removing AC Chair:");
-        System.out.println(passengerBogies);
-
-        if (passengerBogies.contains("Sleeper")) {
-            System.out.println("\nSleeper bogie exists in the train.");
-        } else {
-            System.out.println("\nSleeper bogie does not exist in the train.");
-        }
-
-        System.out.println("\nFinal Train Consist:");
-        System.out.println(passengerBogies);
+        System.out.println("Unique Bogie IDs:");
+        System.out.println(bogieIds);
     }
 }
