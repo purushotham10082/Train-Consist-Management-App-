30bf4a99f4b235271b38110fbb0b5b04b389a186
purushotham <purushotham10082.indus@gmail.com>
2026-04-02 14:34:04 +0530

    Add source code for UC1

---
+++ App/src/TrainConsistManagement.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagement {
+
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial state: Train consist initialized.");
+        System.out.println("Current Bogie Count: " + trainConsist.size());
+
+        System.out.println("Ready to add passenger or goods bogies.");
+    }
+}
